3b15f20eefd62e7059a47e39635a00572293154c
CodeByisaac <highzik86@gmail.com>
2026-07-29 03:59:54 -0400

    add Order entity

--- src/main/java/com/codebyisaac/mapi/entity/Order.java
+++ src/main/java/com/codebyisaac/mapi/entity/Order.java
@@ -1,4 +1,30 @@
 package com.codebyisaac.mapi.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy="order", fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderItem> items = new ArrayList<>();
+
 }
