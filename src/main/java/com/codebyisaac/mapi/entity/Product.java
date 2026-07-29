5e7c00664c35a7a23f5101b0240b302cd18d5836
CodeByisaac <highzik86@gmail.com>
2026-07-29 03:59:25 -0400

    add Product entity

--- src/main/java/com/codebyisaac/mapi/entity/Product.java
+++ src/main/java/com/codebyisaac/mapi/entity/Product.java
@@ -1,4 +1,24 @@
 package com.codebyisaac.mapi.entity;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, length=150)
+    private String name;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    @Column(length=100)
+    private String category;
 }
