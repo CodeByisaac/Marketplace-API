dd2bce7f1359379def807b53e118f0d308eb0bbb
CodeByisaac <highzik86@gmail.com>
2026-07-29 04:00:46 -0400

    add OrderItem SOC entity

--- src/main/java/com/codebyisaac/mapi/entity/OrderItem.java
+++ src/main/java/com/codebyisaac/mapi/entity/OrderItem.java
@@ -1,4 +1,29 @@
 package com.codebyisaac.mapi.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="order_id", nullable=false)
+    private Order order;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal priceAtPurchase;
+
 }
