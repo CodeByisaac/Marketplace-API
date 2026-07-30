d66c41fe25822f2b09616492c9c854535af04adc
CodeByisaac <highzik86@gmail.com>
2026-07-30 16:00:20 -0400

    add OrderPlacementRequest DTO

--- src/main/java/com/codebyisaac/mapi/dto/OrderPlacementRequest.java
+++ src/main/java/com/codebyisaac/mapi/dto/OrderPlacementRequest.java
@@ -1,4 +1,14 @@
 package com.codebyisaac.mapi.dto;
 
+import java.util.List;
+
 public class OrderPlacementRequest {
+    private String userId;
+    private List<OrderItemRequest> items;
+
+    public String getUserId() {return userId;}
+    public void setUserID(String userId) {this.userId = userId;}
+
+    public List<OrderItemRequest> getItems() {return items;}
+    public void setItems(List<OrderItemRequest> items) {this.items = items;}
 }
