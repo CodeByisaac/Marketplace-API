a585d4dd3fec6da78cce99885117f0cbeeae467b
CodeByisaac <highzik86@gmail.com>
2026-07-30 02:05:59 -0400

    add findByName query to ProductRepository

--- src/main/java/com/codebyisaac/mapi/repository/ProductRepository.java
+++ src/main/java/com/codebyisaac/mapi/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.codebyisaac.mapi.repository;
 
-public interface ProductRepository {
+import com.codebyisaac.mapi.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByName(String name);
 }
