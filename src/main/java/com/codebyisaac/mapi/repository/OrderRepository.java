691b7e62ea8f0b712c51e2b8e8331eb471ae7dee
CodeByisaac <highzik86@gmail.com>
2026-07-30 02:07:05 -0400

    add FindByUserId query to OrderRepository for list of orders by user

--- src/main/java/com/codebyisaac/mapi/repository/OrderRepository.java
+++ src/main/java/com/codebyisaac/mapi/repository/OrderRepository.java
@@ -1,4 +1,12 @@
 package com.codebyisaac.mapi.repository;
 
-public interface OrderRepository {
+import com.codebyisaac.mapi.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository {
+    List<Order> findByUserId(String userId);
 }
