baff263a416257e16c0d59f9b0ed5db0b15de0e3
CodeByisaac <highzik86@gmail.com>
2026-08-04 02:29:29 -0400

    add OrderResponse DTO

--- src/main/java/com/codebyisaac/mapi/dto/OrderResponse.java
+++ src/main/java/com/codebyisaac/mapi/dto/OrderResponse.java
@@ -1,4 +1,16 @@
 package com.codebyisaac.mapi.dto;
 
+import lombok.Value;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Value
 public class OrderResponse {
+    String id;
+    String userId;
+    BigDecimal totalPrice;
+    LocalDateTime createdAt;
+    List<OrderItemResponse> items;
 }
