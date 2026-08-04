59a381cc429e9e990de3b8d88c4cfdf85fbe5a4a
CodeByisaac <highzik86@gmail.com>
2026-08-04 02:43:21 -0400

    add OrderMapper for entity to response dto

--- src/main/java/com/codebyisaac/mapi/mapper/OrderMapper.java
+++ src/main/java/com/codebyisaac/mapi/mapper/OrderMapper.java
@@ -1,4 +1,26 @@
 package com.codebyisaac.mapi.mapper;
 
+import com.codebyisaac.mapi.dto.OrderItemResponse;
+import com.codebyisaac.mapi.dto.OrderResponse;
+import com.codebyisaac.mapi.entity.Order;
+
+import java.util.List;
+
 public class OrderMapper {
+    public OrderResponse toResponse(Order order) {
+        List<OrderItemResponse> items = order.getItems().stream()
+                .map( i -> new OrderItemResponse(
+                        i.getProduct().getId(),
+                        i.getProduct().getName(),
+                        i.getQuantity(),
+                        i.getPriceAtPurchase()))
+                .toList();
+
+        return new OrderResponse(
+                order.getId(),
+                order.getUser().getId(),
+                order.getTotalPrice(),
+                order.getCreatedAt(),
+                items);
+    }
 }
