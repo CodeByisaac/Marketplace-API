2e77943ac3d914ff06c62ddaee6654a4e2b9b034
CodeByisaac <highzik86@gmail.com>
2026-08-04 02:39:40 -0400

    custom exception for ProductNotFound

--- src/main/java/com/codebyisaac/mapi/exception/ProductNotFoundException.java
+++ src/main/java/com/codebyisaac/mapi/exception/ProductNotFoundException.java
@@ -1,4 +1,7 @@
 package com.codebyisaac.mapi.exception;
 
-public class ProductNotFoundException {
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String id){
+        super("Product not found" + id);
+    }
 }
