494f99e62acb684236e05735afc916a4106d6450
CodeByisaac <highzik86@gmail.com>
2026-08-25 02:48:41 -0400

    update OrderPlacementRequest

--- src/main/java/com/codebyisaac/mapi/dto/OrderPlacementRequest.java
+++ src/main/java/com/codebyisaac/mapi/dto/OrderPlacementRequest.java
@@ -3,21 +3,17 @@ package com.codebyisaac.mapi.dto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Value;
 
 import java.util.List;
 
+@Value
 public class OrderPlacementRequest {
 
     @NotBlank
-    private String userId;
+    String userId;
 
     @NotEmpty
     @Valid
-    private List<OrderItemRequest> items;
-
-    public String getUserId() {return userId;}
-    public void setUserId(String userId) {this.userId = userId;}
-
-    public List<OrderItemRequest> getItems() {return items;}
-    public void setItems(List<OrderItemRequest> items) {this.items = items;}
+    List<OrderItemRequest> items;
 }
