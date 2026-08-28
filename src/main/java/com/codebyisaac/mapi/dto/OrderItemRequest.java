976d61f6cd9737afa65893e824cf7944337a2d8f
CodeByisaac <highzik86@gmail.com>
2026-08-28 16:38:33 -0400

    convert OrderItemRequest class to record class

--- src/main/java/com/codebyisaac/mapi/dto/OrderItemRequest.java
+++ src/main/java/com/codebyisaac/mapi/dto/OrderItemRequest.java
@@ -2,15 +2,8 @@ package com.codebyisaac.mapi.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Value;
 import org.antlr.v4.runtime.misc.NotNull;
 
-@Value
-public class OrderItemRequest {
-    @NotBlank
-    String productId;
-
-    @NotNull
-    @Min(value = 1, message="quantity must be at least 1")
-    Integer quantity;
+public record OrderItemRequest(@NotBlank String productId,
+                               @NotNull @Min(value = 1, message = "quantity must be at least 1") Integer quantity) {
 }
