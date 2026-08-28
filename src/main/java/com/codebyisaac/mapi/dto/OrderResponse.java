b69b58f4534e0396eebbd58736c469f9c90c220f
CodeByisaac <highzik86@gmail.com>
2026-08-28 16:41:51 -0400

    convert OrderResponse class to record class

--- src/main/java/com/codebyisaac/mapi/dto/OrderResponse.java
+++ src/main/java/com/codebyisaac/mapi/dto/OrderResponse.java
@@ -1,16 +1,9 @@
 package com.codebyisaac.mapi.dto;
 
-import lombok.Value;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Value
-public class OrderResponse {
-    String id;
-    String userId;
-    BigDecimal totalPrice;
-    LocalDateTime createdAt;
-    List<OrderItemResponse> items;
+public record OrderResponse(String id, String userId, BigDecimal totalPrice, LocalDateTime createdAt,
+                            List<OrderItemResponse> items) {
 }
