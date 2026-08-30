ac3f76fbc12b40f67ddee9e235e821fb40b6f827
CodeByisaac <highzik86@gmail.com>
2026-08-30 15:24:29 -0400

    Update OrderRepostiory with JOIN FETCH

--- src/main/java/com/codebyisaac/mapi/repository/OrderRepository.java
+++ src/main/java/com/codebyisaac/mapi/repository/OrderRepository.java
@@ -2,11 +2,17 @@ package com.codebyisaac.mapi.repository;
 
 import com.codebyisaac.mapi.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
-    List<Order> findByUserId(String userId);
+    @Query("SELECT o FROM Order o JOIN FETCH o.items")
+    List<Order> findAllWithItems();
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.items WHERE o.userId = :userId")
+    List<Order> findByUserIdWithItems(@Param("userId") String userId);
 }
