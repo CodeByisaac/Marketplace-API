cc73e5e92a44908d8dd473861c307d6d0867d1b6
CodeByisaac <highzik86@gmail.com>
2026-09-02 15:51:56 -0400

    update with @Jacksonized annotation

--- src/main/java/com/codebyisaac/mapi/dto/APIResponse.java
+++ src/main/java/com/codebyisaac/mapi/dto/APIResponse.java
@@ -3,11 +3,13 @@ package com.codebyisaac.mapi.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Value
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Jacksonized
 @Builder
 public class APIResponse<T> {
      String status;
